cf991a769626ece40c70c97ee74fe4d6dfebe3a2
Mohammed Amine <boutouilmohammedamine@gmail.com>
2021-04-11 11:00:53 +0100

    clean up

--- api-gateway/scripts/reload.sh
+++ api-gateway/scripts/reload.sh
@@ -4,7 +4,7 @@ while true; do
   inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/conf.d
   nginx -t
   if [ $? -eq 0 ]; then
-    echo "Detected Nginx Configuration Change"
+    echo "Reloading nginx with new configuration"
     echo "Executing: nginx -s reload"
     nginx -s reload
   fi
